e549d867de9dc6a115aeaad6297f42e80c1ac701
Roy Bekkering <roytazz@gmail.com>
2016-04-21 01:23:59 +0200

    Added missing guild api properties

--- GuildWars2API/Model/Guild/LogEntry.cs
+++ GuildWars2API/Model/Guild/LogEntry.cs
@@ -6,7 +6,7 @@ namespace GuildWars2API.Model.Guild
     public class LogEntry
     {
         [JsonProperty("id")]
-        public int ID { get; set; }
+        public int LogID { get; set; }
 
         /// <summary>
         /// ISO-8601 format
@@ -19,13 +19,28 @@ namespace GuildWars2API.Model.Guild
 
         /// <summary>
         /// Possible values (value, explanation):
-        /// Treasury - A guild member has deposited an item into the guild's treasury. Additonal fields include:
+        /// Stash - A guild member has deposited an item into the guild's treasury. Additonal fields include:
         ///     ItemID - The item ID that was deposited into the treasury
         ///     Count - How many of the specified item was deposited
+        ///     Operation - (deposit, withdraw)
         /// MOTD - A guild member has changed the guild's MOTD. Additional fields include:
         ///     MOTD - The new MOTD
+        /// Invited - A new person has joined the guild. Additional fields include:
+        ///     InvitedBy - The user that invited this person
+        /// Upgrade - An upgrade has been performed. Additional fields include:
+        ///     Action - (complete, cancelled, qeued)
+        ///     UpgradeID
         [JsonProperty("type")]
         public string Type { get; set; }
+
+        [JsonProperty("operation")]
+        public string Operation { get; set; }
+
+        [JsonProperty("upgrade_id")]
+        public string UpgradeID { get; set; }
+
+        [JsonProperty("invited_by")]
+        public string InvitedBy { get; set; }
 
         [JsonProperty("item_id")]
         public int ItemID { get; set; }
@@ -38,5 +53,8 @@ namespace GuildWars2API.Model.Guild
 
         [JsonProperty("motd")]
         public string MOTD { get; set; }
+
+        [JsonProperty("action")]
+        public string Action { get; set; }
     }
 }
