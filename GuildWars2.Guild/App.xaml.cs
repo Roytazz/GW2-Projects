66ffa3d9bc50154169a5fc58273f8eb57e34f667
Roy Bekkering <roytazz@gmail.com>
2016-04-21 23:47:00 +0200

    Added auto update and updates usercontrols

--- GuildWars2.Guild/App.xaml.cs
+++ GuildWars2.Guild/App.xaml.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using GuildWars2Guild.Classes;
 using System.Windows;
 
-namespace GuildWars2.Guild
+namespace GuildWars2Guild
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e) {
+            foreach(var flag in e.Args) {
+                if(flag.Contains("update")) {
+                    Updater.Update();
+                    Shutdown();
+                }
+            }
+            base.OnStartup(e);
+        }
     }
 }
--- GuildWars2.Guild/Model/StashViewModel.cs
+++ GuildWars2.Guild/Model/StashViewModel.cs
@@ -2,18 +2,36 @@
 using GuildWars2Guild.Classes;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace GuildWars2Guild.Model
 {
-    class StashViewModel
+    class StashViewModel : INotifyPropertyChanged
     {
         private List<LogEntry> _log;
-        public ObservableCollection<GoldEntry> GoldLogEntries { get; set; }
+        private GoldEntry _selectedRow;
+
+        public ObservableCollection<GoldEntry> GoldLogMember {
+            get {
+                if(SelectedRow != null) {
+                    var results = new ObservableCollection<GoldEntry>(GetGoldEntries().Where(entry => entry.LogEntry.User.Equals(SelectedRow.LogEntry.User)).ToList());
+                    return results;
+                }
+                return new ObservableCollection<GoldEntry>();
+            }
+        }
+        public ObservableCollection<GoldEntry> GoldLogTotal { get; set; }
+
+        public GoldEntry SelectedRow {
+            get { return _selectedRow; }
+            set { _selectedRow = value; NotifyPropertyChanged("GoldLogMember"); }
+        }
 
         public StashViewModel() {
             _log = DBManager.GetLogEntries();
-            GoldLogEntries = new ObservableCollection<GoldEntry>(GetGoldEntries());
+            GoldLogTotal = new ObservableCollection<GoldEntry>(GetGoldEntries());
         }
 
         private List<GoldEntry> GetGoldEntries() {
@@ -24,6 +42,14 @@ namespace GuildWars2Guild.Model
                 goldEntries.Add(new GoldEntry() { LogEntry = goldEntry });
             }
             return goldEntries;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "") {
+            if(PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
