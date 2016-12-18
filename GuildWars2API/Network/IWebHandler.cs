1d29179ba1b27767fa34c96bd8d7d20405de9576
Roytazz <roytazz@gmail.com>
2016-12-18 21:35:12 +0100

    Removed unused Projects

--- GuildWars2API/Network/IWebHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace GuildWars2API.Network
-{
-    public interface IWebHandler
-    {
-        T GetRequest<T>(string url, Dictionary<string, string> headers);
-    }
-}
