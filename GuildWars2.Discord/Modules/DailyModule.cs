2d723b2af7301edb6f542462d8f6bd282728e696
roytazz <roytazz@gmail.com>
2017-07-31 22:21:54 +0200

    Added a Discord Bot

---
+++ GuildWars2.Discord/Modules/DailyModule.cs
@@ -0,0 +1,34 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GuildWars2.Discord.Services;
+
+namespace GuildWars2.Discord.Modules
+{
+    public class DailyModule : ModuleBase
+    {
+        private ApiService _api;
+
+        public DailyModule(ApiService api) {
+            _api = api;
+        }
+
+        [Command("daily")]
+        public async Task ListDailies()
+        {
+            await ReplyAsync(await _api.Dailies());  
+        }
+
+        [Command("daily+")]
+        public async Task ListTomorrowDailies() {
+            await ReplyAsync(await _api.TomorrowDailies());
+        }
+        
+        [Command("dailyTomorrow")]
+        public async Task ListTomorrowDailiesOverload() {
+            await ListTomorrowDailies();
+        }
+    }
+}
