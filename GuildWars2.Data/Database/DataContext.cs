e02b11f112957cb947c4e627ead7c961b0119973
roytazz <roytazz@gmail.com>
2018-05-09 19:04:13 +0200

    Added new lib for value calculations

---
+++ GuildWars2.Data/Database/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using GuildWars2.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuildWars2.Data.Database
+{
+    public class DataContext : DbContext {
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            modelBuilder.HasDefaultSchema(schema: DbGlobals.SchemaName);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public static class DbGlobals {
+        public const string SchemaName = "GuildWars2.Data";
+    }
+
+    public class DataContextFactory : IDesignTimeDbContextFactory<DataContext> {
+        public DataContext CreateDbContext(string[] args) {
+            var builder = new DbContextOptionsBuilder<DataContext>();
+            builder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=config;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new DataContext(builder.Options);
+        }
+    }
+}
---
+++ GuildWars2.Data/Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GuildWars2.Data.Model
+{
+    public class User
+    {
+        [Key]
+        public string Key { get; set; }
+    }
+}
