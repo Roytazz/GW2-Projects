e55819e1473d0a33a981655b1b0cfc1f9ddc3727
roytazz <roytazz@gmail.com>
2018-05-18 17:11:33 +0200

    Added valueservice for Recipes

---
+++ GuildWars2.API/Model/Items/ItemRecipeTreeNode.cs
@@ -0,0 +1,103 @@
+﻿using GuildWars2.API.Model.Items;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuildWars2.API.Items
+{
+    public class ItemRecipeTreeNode
+    {
+        public int ItemID { get; set; }
+
+        public int ItemCount { get; set; }
+
+        public List<ItemRecipeTreeNode> Children { get; set; }
+
+        private List<Recipe> _recipes;
+        private int _currentRecipeIndex = 0;
+
+        public ItemRecipeTreeNode(int itemID, int itemCount = 1) {
+            _recipes = new List<Recipe>();
+            Children = new List<ItemRecipeTreeNode>();
+            ItemID = itemID;
+            ItemCount = itemCount;
+        }
+
+        public async Task ChangeRecipe() {
+            if (_recipes.Count <= 1)
+                return;
+
+            var newIndex = _currentRecipeIndex + 1;
+            if(newIndex >= _recipes.Count) {
+                _currentRecipeIndex = 0;
+            }
+            else {
+                _currentRecipeIndex = newIndex;
+            }
+            await Populate();
+        }
+
+        /// <summary>
+        /// Used to override a recipe or select a specific one.
+        /// </summary>
+        /// <param name="recipeID"></param>
+        /// <returns></returns>
+        public async Task ChangeRecipe(int recipeID) {
+            if (_recipes.Any(x => x.ID == recipeID)) {
+                _currentRecipeIndex = _recipes.IndexOf(_recipes.FirstOrDefault(x => x.ID == recipeID));
+            }
+            else {
+                var newRecipe = await ItemAPI.Recipes(recipeID);
+                _recipes = new List<Recipe> { newRecipe };
+                _currentRecipeIndex = 0;
+            }
+            await Populate();
+        }
+
+        public Recipe CurrentRecipe() {
+            return _recipes[_currentRecipeIndex];
+        }
+
+        public async Task Populate() {
+            await PopulateRecipe();
+            await PopulateChildren();
+        }
+
+        private async Task PopulateRecipe() {
+            var _recipeIDs = await ItemAPI.SearchRecipesByOutput(ItemID);
+            if (_recipeIDs?.Count > 0) {                                                                //Check for normal recipe
+                _recipes = await ItemAPI.Recipes(_recipeIDs);
+            }
+            /*else if (!ItemAPI.IsPromotionItem(ItemID)) {                                              //Check for Mystic Forge recipe
+                var mysticForgeRecipes = await ItemAPI.SearchMysticForgeRecipesByOutput(ItemID);
+
+                if (mysticForgeRecipes?.Count > 0) {
+                    _recipe = mysticForgeRecipes[0];
+                    foreach (var recipe in mysticForgeRecipes) {
+                        _recipeIDs.Add(recipe.ID);
+                    }
+                }
+            }*/
+            /*else {                                                                                    //This should onlu occur when the item is a consumable recipe
+                if (_item == null)
+                    _item = await ItemAPI.Items(ItemID);
+
+                if (_item?.Details != null && _item?.Details.RecipeID != 0) {          
+                    _recipeIDs.Add(_item.Details.RecipeID);                                              
+                    _recipe = await ItemAPI.Recipes(_item.Details.RecipeID);
+                }
+            }*/
+        }
+
+        private async Task PopulateChildren() {
+            if (_recipes?.Count <= _currentRecipeIndex || _recipes?[_currentRecipeIndex] == null)
+                return;
+
+            foreach (RecipeIngredient ingredient in _recipes[_currentRecipeIndex].Ingredients) {
+                var node = new ItemRecipeTreeNode(ingredient.ItemID, ItemCount * ingredient.Count);
+                await node.Populate();
+                Children.Add(node);
+            }
+        }
+    }
+}
--- GuildWars2.Worker/ValueService/RecipeValueService.cs
+++ GuildWars2.Worker/ValueService/RecipeValueService.cs
@@ -4,22 +4,78 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using System.Linq;
+using GuildWars2.API.Items;
+using GuildWars2.API;
+using GuildWars2.API.Model.Commerce;
 
 namespace GuildWars2.Worker.ValueService
 {
-    class RecipeValueService : IValueService<ItemRecipeTreeNode>
+    public class RecipeValueService : IValueService<ItemRecipeTreeNode>
     {
-        public async Task<ValueResult<ItemRecipeTreeNode>> CalculateValueAsync(ItemRecipeTreeNode item) {
-            var result = await CalculateValue(new List<ItemRecipeTreeNode> { item });
+        public async Task<ValueResult<ItemRecipeTreeNode>> CalculateValue(ItemRecipeTreeNode item, bool takeHighestValue) {
+            var result = await CalculateValue(new List<ItemRecipeTreeNode> { item }, takeHighestValue);
             return result.FirstOrDefault();
         }
 
-        public Task<List<ValueResult<ItemRecipeTreeNode>>> CalculateValue(List<ItemRecipeTreeNode> items) {
-            throw new NotImplementedException();    //TODO Calculate value of recipe (need to compare all available recipes to find the lowest/higest value recipe)
+        public async Task<List<ValueResult<ItemRecipeTreeNode>>> CalculateValue(List<ItemRecipeTreeNode> items, bool takeHighestValue) {
+            var result = new List<ValueResult<ItemRecipeTreeNode>>();
+            foreach (var item in items) {
+                var itemIDs = GetItemIDs(item).Select(x=>x.ItemID).Distinct().ToList();
+                var listings = await CommerceAPI.ListingsAggregated(itemIDs);
+
+                var itemPrice = new ItemPrice();
+                result.Add(new ValueResult<ItemRecipeTreeNode> { Item = item, Value = GetValue(item, takeHighestValue, listings) });
+            }
+            return result;    
         }
 
         public bool IsApplicable(ItemRecipeTreeNode item) {
             return true;
         }
+
+        private List<ItemAmountHelper> GetItemIDs(ItemRecipeTreeNode node) {
+            var itemIDs = new List<ItemAmountHelper> {
+                new ItemAmountHelper { ItemID = node.ItemID, Amount = node.ItemCount }
+            };
+            foreach (var childNode in node.Children) {
+                itemIDs.AddRange(GetItemIDs(childNode));
+            }
+            return itemIDs;
+        }
+
+        private ItemPrice GetValue(ItemRecipeTreeNode node, bool takeHighestValue, List<ItemListingAggregated> listings) {      //Math is slighty off compared to the Wiki.
+            ItemPrice itemPrice = null;                                                                                         //Might be bcs the wiki uses vendor prices and we only use TP prices.
+            if (listings.Any(x => x.ItemID == node.ItemID)) {                                                                   //Will improve further with visual representation eventually.
+                if(takeHighestValue)
+                    itemPrice = listings.Find(x => x.ItemID == node.ItemID).Sells.Price;
+                else
+                    itemPrice = listings.Find(x => x.ItemID == node.ItemID).Buys.Price;
+            }
+
+            ItemPrice allChildrenValue = null;
+            foreach (var child in node.Children) {
+                var childValue = GetValue(child, takeHighestValue, listings);
+                if (allChildrenValue == null && childValue != null)
+                    allChildrenValue = childValue;
+                else if (allChildrenValue != null && childValue != null)
+                    allChildrenValue = new ItemPrice(allChildrenValue.Coins + childValue.Coins);
+            }
+
+            if (itemPrice == null && allChildrenValue != null)
+                itemPrice = allChildrenValue;
+            else if (itemPrice == null && allChildrenValue == null)
+                return null;
+            else if (allChildrenValue != null && itemPrice > allChildrenValue && !takeHighestValue)
+                itemPrice = allChildrenValue;
+
+
+            return itemPrice != null ? new ItemPrice(itemPrice.Coins * node.ItemCount) : null;
+        }
+    }
+    
+    internal class ItemAmountHelper
+    {
+        public int ItemID { get; set; }
+        public int Amount { get; set; }
     }
 }
