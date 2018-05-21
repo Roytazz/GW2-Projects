dd339ce990724d95f63cdcf66ce1a850e9a1046e
roytazz <roytazz@gmail.com>
2018-05-21 19:47:10 +0200

    Simplified ValueResult

---
+++ GuildWars2.Value/ValueResult.cs
@@ -0,0 +1,12 @@
+﻿using GuildWars2.API.Model.Commerce;
+using System;
+
+namespace GuildWars2.Value
+{
+    public class ValueResult<T>
+    {
+        public T Item { get; set; }
+
+        public ItemPrice Value { get; set; }
+    }
+}
