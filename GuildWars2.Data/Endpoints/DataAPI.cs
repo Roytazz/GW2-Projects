88a4ebc486e1c8cdbf827eeb6cc779627e6c83fc
roytazz <roytazz@gmail.com>
2018-05-21 22:46:08 +0200

    Added DataAPI endpoint

--- GuildWars2.Data/Endpoints/DataAPI.cs
+++ GuildWars2.Data/Endpoints/DataAPI.cs
@@ -1,5 +1,7 @@
 ﻿using GuildWars2.API.Model.Items;
 using GuildWars2.Data.Database;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +15,41 @@ namespace GuildWars2.Data.Endpoints
         public static async Task AddItems(List<Item> items) {
             using(var db = new DataContextFactory().CreateDbContext()) {
                 foreach (var item in items) {
-                    if (!db.Item.Any(x => x.ID == item.ID)) //Implement UPDATE and Equals()
+                    if (db.Item.Any(x => x.ID == item.ID)) {
+                        var foundItem = await db.Item.FirstOrDefaultAsync(x => x.ID == item.ID);
+                        if (!foundItem.IsEqual(item)) {
+                            var dbItem = await db.Item.FirstOrDefaultAsync(x => x.ID == item.ID);
+                            db.Item.Remove(dbItem);
+                            db.Item.Add(item);
+                        }
+                    }
+                    else
                         db.Item.Add(item);
                 }
                 await db.SaveChangesAsync();
             }
+        }
+
+        public static async Task<List<Item>> GetItems(List<int> itemIDs) {
+            using (var db = new DataContextFactory().CreateDbContext()) {
+                return await db.Item.Where(x => itemIDs.Contains(x.ID)).ToListAsync();
+            }
+        }
+
+        public static async Task RemoveItems(List<Item> items) {
+            using (var db = new DataContextFactory().CreateDbContext()) {
+                var dbItems = await db.Item.Where(x => items.Select(y => y.ID).Contains(x.ID)).ToListAsync();
+                db.Item.RemoveRange(dbItems);
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+
+    public static class ItemExtension
+    {
+        public static bool IsEqual(this Item obj, Item item) {
+            //TODO
+            return true;
         }
     }
 }
