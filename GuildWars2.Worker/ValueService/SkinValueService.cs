7d40f9028657a6d3ff5b24c3c92c5048f74d0956
roytazz <roytazz@gmail.com>
2018-05-22 23:28:25 +0200

    Finished SkinValueService

--- GuildWars2.Worker/ValueService/SkinValueService.cs
+++ GuildWars2.Worker/ValueService/SkinValueService.cs
@@ -2,11 +2,8 @@
 using GuildWars2.API.Model.Items;
 using GuildWars2.Data.Endpoints;
 using GuildWars2.Value;
-using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace GuildWars2.Worker.ValueService
