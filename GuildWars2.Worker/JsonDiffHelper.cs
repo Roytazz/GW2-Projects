d9f1b023596a36716b6b90f77566d8b59812f213
roytazz <roytazz@gmail.com>
2018-05-26 18:40:43 +0200

    Created Worker to retrieve all items and listings + comparing them to existing Items.

---
+++ GuildWars2.Worker/JsonDiffHelper.cs
@@ -0,0 +1,34 @@
+﻿using JsonDiffPatchDotNet;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace GuildWars2.Worker
+{
+    internal static class JsonDiffConsoleHelper
+    {
+        public static bool DiffObject<T>(T obj1, T obj2, bool waitForUser = true) {
+            var jdp = new JsonDiffPatch();
+
+            var leftString = JsonConvert.SerializeObject(obj1);
+            var rightString = JsonConvert.SerializeObject(obj2);
+
+            var left = JToken.FromObject(obj1);
+            var right = JToken.FromObject(obj2);
+            JToken patch = jdp.Diff(left, right);
+
+            if (patch != null) {
+                if (!waitForUser)
+                    return true;
+
+                Console.WriteLine(patch.ToString());
+                Console.WriteLine();
+                Console.WriteLine("Type 'Y' to replace current item.");
+                var userResponse = Console.ReadKey();
+                if (userResponse.Key == ConsoleKey.Y)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ GuildWars2.Worker/Worker/DataWorkers/ItemDataWorker.cs
@@ -0,0 +1,64 @@
+﻿using GuildWars2.API;
+using GuildWars2.API.Model.Items;
+using GuildWars2.Data.Endpoints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GuildWars2.Worker.DataWorker
+{
+    public class ItemDataWorker : IDataWorker
+    {
+        private static readonly int MAX_ITEM_PER_PAGE = 200;
+
+        public async Task Run(CancellationToken token) {
+            var page = 228;
+            Write(nameof(ItemDataWorker) + " starting...");
+            while (true) {
+                Write("Requesting Page #" + page + "...");
+                var pageResult = await ItemAPI.Items(MAX_ITEM_PER_PAGE, page);
+                var itemIDs = pageResult.Select(x => x.ID).ToList();
+                var dbItems = await DataAPI.GetItems(itemIDs);
+                var newItems = GetNewItems(pageResult, dbItems);
+
+                Write("Retrieved " + newItems.Count + " new/changed items. Saving to DB...");
+
+                await DataAPI.AddItems(newItems);
+                Write("Saving completed! Requesting ItemListings...");
+
+                var listings = await CommerceAPI.ListingsAggregated(pageResult.Select(x => x.ID).ToList());
+
+                Write("Done Requesting ItemListings. Saving to DB...");
+
+                await DataAPI.AddItemSellable(pageResult, listings);
+                Write("Saving completed!");
+
+                page++;
+                if (pageResult.Count < 200)
+                    break;
+            }
+            Write(string.Empty);
+            Write(nameof(ItemDataWorker) + " done.");
+        }
+
+        private void Write(string msg) {
+            Console.WriteLine(msg);
+        }
+
+        private List<Item> GetNewItems(List<Item> apiItems, List<Item> dbItems) {
+            var result = new List<Item>();
+            foreach (var apiItem in apiItems) {
+                if (dbItems.Any(x => x.ID == apiItem.ID)) {
+                    var isNew = JsonDiffConsoleHelper.DiffObject(apiItem, dbItems.FirstOrDefault(x => x.ID == apiItem.ID));
+                    if (isNew)
+                        result.Add(apiItem);
+                }
+                else
+                    result.Add(apiItem);
+            }
+            return result;
+        }
+    }
+}
