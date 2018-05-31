d9d2cf5185d9f05767e3bcbb0f2a057f204264fb
roytazz <roytazz@gmail.com>
2018-05-31 18:34:13 +0200

    Added Crafting Value Service

---
+++ GuildWars2.Worker/ValueService/CraftingValueService.cs
@@ -0,0 +1,43 @@
+﻿using GuildWars2.API.Items;
+using GuildWars2.API.Model;
+using GuildWars2.API.Model.Items;
+using GuildWars2.Value;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuildWars2.Worker.ValueService
+{
+    class CraftingValueService : IValueService<Item>
+    {
+        public async Task<ValueResult<Item>> CalculateValue(Item entity, bool takeHighestValue) {
+            var result = await CalculateValue(new List<Item> { entity }, takeHighestValue);
+            return result.FirstOrDefault();
+        }
+
+        public async Task<List<ValueResult<Item>>> CalculateValue(List<Item> entities, bool takeHighestValue) {
+            var recipes = new List<ItemRecipeTreeNode>();
+            foreach (var item in entities) {
+                var recipe = new ItemRecipeTreeNode(item.ID);
+                await recipe.Populate();
+                recipes.Add(recipe);
+            }
+
+            var result = new List<ValueResult<Item>>();
+            var values = await ValueFactory.CalculateValue(recipes, takeHighestValue);
+            foreach (var item in entities) {
+                result.Add(new ValueResult<Item> {
+                    Item = item,
+                    Value = values.FirstOrDefault(x => x.Item.ItemID == item.ID)?.Value
+                });
+            }
+            return result;
+        }
+
+        public bool IsApplicable(Item item) {
+            return item.Rarity == ItemRarity.Ascended;
+        }
+    }
+}
