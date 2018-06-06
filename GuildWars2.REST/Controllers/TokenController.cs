b484be1d8dfa3777f53846e56fed4e3d1f78c5a8
roytazz <roytazz@gmail.com>
2018-06-06 16:15:38 +0200

    Added Token-base GW2 REST API

---
+++ GuildWars2.REST/Controllers/TokenController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GuildWars2.REST.Controllers
+{
+    [Route("api/[controller]")]
+    public class TokenController : Controller
+    {
+        private IConfiguration _config;
+
+        public TokenController(IConfiguration config) {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult CreateToken([FromBody]LoginModel login) {
+            if (Authenticate(login))
+                return Ok(new { token = BuildToken() });
+            else
+                return Unauthorized();
+        }
+
+        private string BuildToken() {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Issuer"],
+              expires: DateTime.Now.AddDays(1),
+              signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private bool Authenticate(LoginModel login) {
+            return login.Password.Equals(_config["GW2:Password"]);
+        }
+
+        public class LoginModel
+        {
+            public string Password { get; set; }
+        }
+    }
+}
