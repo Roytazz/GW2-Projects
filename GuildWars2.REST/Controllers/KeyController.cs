d53405662e5bfabd453e511d52b8e12704ffe1e1
roytazz <roytazz@gmail.com>
2018-06-06 16:47:01 +0200

    Added ApiKey Controller

---
+++ GuildWars2.REST/Controllers/KeyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using GuildWars2.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuildWars2.REST.Controllers
+{
+    [Route("api/[controller]")]
+    public class KeyController : Controller
+    {
+        [Authorize, HttpPost]
+        public async Task<IActionResult> AddKey([FromBody]APIKey key)
+        {
+            await UserAPI.AddUser(key.Key);
+            return Ok();
+        }
+    }
+
+    public class APIKey
+    {
+        public string Key { get; set; }
+    }
+}
