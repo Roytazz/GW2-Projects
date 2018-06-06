73397717a0556b33b53de48b5dc2fc2a5573c052
roytazz <roytazz@gmail.com>
2018-06-06 02:44:16 +0200

    Slighty rewrote DiffMatcher and added visualization

--- GuildWars2.Worker/JsonDiffHelper.cs
+++ GuildWars2.Worker/JsonDiffHelper.cs
@@ -2,33 +2,80 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
 
 namespace GuildWars2.Worker
 {
-    internal static class JsonDiffConsoleHelper
+    internal class DiffConsoleHelper<T>
     {
-        public static bool DiffObject<T>(T obj1, T obj2, bool waitForUser = true) {
-            var jdp = new JsonDiffPatch();
+        private static readonly string FILE_LOCATION = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "preview.htm");
 
-            var leftString = JsonConvert.SerializeObject(obj1);
-            var rightString = JsonConvert.SerializeObject(obj2);
+        private T _obj1;
+        private T _obj2;
+        private bool _waitForUser;
 
-            var left = JToken.FromObject(obj1);
-            var right = JToken.FromObject(obj2);
-            JToken patch = jdp.Diff(left, right);
+        public DiffConsoleHelper(T obj1, T obj2, bool waitForUserInput = true) {
+            _obj1 = obj1;
+            _obj2 = obj2;
+            _waitForUser = waitForUserInput;
+        }
+
+        public bool DiffObject() {
+            var left = JToken.FromObject(_obj1);
+            var right = JToken.FromObject(_obj2);
+            
+            JToken patch = new JsonDiffPatch().Diff(left, right);
 
             if (patch != null) {
-                if (!waitForUser)
+                if (!_waitForUser)
                     return true;
 
                 Console.WriteLine(patch.ToString());
                 Console.WriteLine();
-                Console.WriteLine("Type 'Y' to replace current item.");
-                var userResponse = Console.ReadKey();
-                if (userResponse.Key == ConsoleKey.Y)
-                    return true;
+                Console.WriteLine("Type 'Y' to replace item. Type 'V' to show changes.");
+                HandleUserResponse();
             }
             return false;
+        }
+
+        private bool HandleUserResponse() {
+            var userResponse = Console.ReadKey();
+            switch (userResponse.Key) {
+                case ConsoleKey.Y:
+                    return true;
+                case ConsoleKey.V:
+                    ShowChanges();
+                    return HandleUserResponse();
+                default:
+                    return false;
+            }
+        }
+
+        private void ShowChanges() {
+            BuildHtmlPage();
+
+            Process p = new Process();
+            p.StartInfo.FileName = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+            p.StartInfo.Arguments = Uri.EscapeDataString(FILE_LOCATION);
+            p.Start();
+        }
+
+        private void BuildHtmlPage() {
+            var html = new StringBuilder();
+            html.Append("<html><head><style>");
+            html.Append("table{border:1pxsolid#d9d9d9;}     td{border:1pxsolid#d9d9d9;padding:3px;}     ins{background-color:#cfc;text-decoration:inherit;}     del{color:#999;background-color:#FEC8C8;}     ins.mod{background-color:#FFE1AC;}");
+            html.Append("</style></head><body><div>");
+            html.Append(new HtmlDiff.HtmlDiff(JsonConvert.SerializeObject(_obj1), JsonConvert.SerializeObject(_obj2)).Build());
+            html.Append("</div></body></html>");
+
+            using (FileStream fs = new FileStream(FILE_LOCATION, FileMode.Create)) {
+                using (StreamWriter w = new StreamWriter(fs, Encoding.UTF8)) {
+                    w.WriteLine(html);
+                }
+            }
         }
     }
 }
