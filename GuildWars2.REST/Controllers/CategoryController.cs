356df250a4fd4128624ae42c47c078db8c45a520
roytazz <roytazz@gmail.com>
2018-06-11 03:49:17 +0200

    Added account item actions

--- GuildWars2.REST/Controllers/CategoryController.cs
+++ GuildWars2.REST/Controllers/CategoryController.cs
@@ -13,8 +13,8 @@ namespace GuildWars2.REST.Controllers.Value
     [Route("api/v1/[controller]")]
     public class CategoriesController : BaseController
     {
-        [Authorize, HttpGet("all")]
-        public IActionResult All() {
+        [Authorize, HttpGet]
+        public IActionResult Get() {
             var result = new List<object>();
             foreach (var item in Enum.GetNames(typeof(CategoryValueType))) {
                 result.Add(new { id = (int)Enum.Parse(typeof(CategoryValueType), item), type = item });
@@ -22,8 +22,8 @@ namespace GuildWars2.REST.Controllers.Value
             return Ok(result);
         }
 
-        [Authorize, HttpPost]
-        public async Task<IActionResult> Get([FromBody]CategoryModel category) {
+        [Authorize, HttpPost("values")]
+        public async Task<IActionResult> Values([FromBody]CategoriesModel category) {
             if (category == null || category.CategoriesList == null || category.CategoriesList.Count <= 0)
                 return BadRequest(ErrorMessage("categories are invalid/missing"));
 
@@ -31,21 +31,47 @@ namespace GuildWars2.REST.Controllers.Value
             return Ok(categoryValues);
         }
 
-        [Authorize, HttpPost("history")]
-        public async Task<IActionResult> History([FromBody]CategoryHistoryModel category, [FromQuery]int page = 0, [FromQuery]int pageSize = 200) {
+        [Authorize, HttpPost("values/history")]
+        public async Task<IActionResult> ValueHistory([FromBody]CategoryModel category, [FromQuery]int page = 0, [FromQuery]int pageSize = 200) {
             if (category == null)
                 return BadRequest(ErrorMessage("category is missing"));
 
             var categoryValues = await UserAPI.GetCategoryHistory(category.Category, category.APIKey, page, pageSize);
             return Ok(categoryValues);
         }
+
+        [Authorize, HttpPost("items")]
+        public async Task<IActionResult> Items([FromBody]CategoryModel category, [FromQuery]int page = 0, [FromQuery]int pageSize = 200) {
+            if (category == null)
+                return BadRequest(ErrorMessage("category is missing"));
+
+            switch (category.Category) {
+                case CategoryValueType.Default:
+                    return BadRequest(ErrorMessage("category is invalid"));
+                case CategoryValueType.Skins:
+                    return Ok(await UserAPI.GetSkins(category.APIKey, page, pageSize));
+                case CategoryValueType.Dyes:
+                    return Ok(await UserAPI.GetDyes(category.APIKey, page, pageSize));
+                case CategoryValueType.Minis:
+                    return Ok(await UserAPI.GetMinis(category.APIKey, page, pageSize));
+                default:
+                    return Ok(await UserAPI.GetAccountItems(category.Category, category.APIKey, page, pageSize));
+            }
+        }
     }
 
     public class CategoryModel
     {
-        public int[] Categories { get; set; }
-
         public string APIKey { get; set; }
+
+        public CategoryValueType Category { get; set; }
+    }
+
+    public class CategoriesModel
+    {
+        public string APIKey { get; set; }
+
+        public int[] Categories { get; set; }
 
         [JsonIgnore]
         public List<CategoryValueType> CategoriesList {
@@ -53,12 +79,5 @@ namespace GuildWars2.REST.Controllers.Value
                 return Categories.Select(x => (CategoryValueType)x).ToList();
             }
         }
-    }
-
-    public class CategoryHistoryModel
-    {
-        public string APIKey { get; set; }
-
-        public CategoryValueType Category { get; set; }
     }
 }
